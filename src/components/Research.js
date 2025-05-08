a43bbc15cd2fd44d9161b5cae809d2c4ff9581ad
Naveen Sudharsan <57706774+nvnsudharsan@users.noreply.github.com>
2025-05-07 22:06:40 -0500

    Update Research.js
    updated the image link

--- src/components/Research.js
+++ src/components/Research.js
@@ -8,43 +8,43 @@ const projects = [
   {
     title: 'SOLWEIG-GPU: GPU-Accelerated Thermal Comfort Modeling Framework',
     description: 'SOLWEIG-GPU is a Python package and command-line interface for running the SOLWEIG (Solar and LongWave Environmental Irradiance Geometry) model on CPU or GPU (if available). It enables high-resolution urban microclimate modeling by computing key variables such as Sky View Factor (SVF), Mean Radiant Temperature (Tmrt), and the Universal Thermal Climate Index (UTCI)',
-    image: '/images/UTCI_New_Delhi.jpeg',
+    image: '${process.env.PUBLIC_URL}/images/UTCI_New_Delhi.jpeg',
     link: 'https://github.com/nvnsudharsan/SOLWEIG-GPU'
   },
   {
     title: 'Comparison of AI and Physics based models for Hurricane Track Prediction at Realtime',
     description: 'Improving hurricane track prediction with deep learning.',
-    image: '/images/hurricane.png',
+    image: '${process.env.PUBLIC_URL}/images/hurricane.png',
     link: '/research/hurricane'
   },
   {
     title: 'Heat Risk Assessment',
     description: 'High-resolution flood prediction using coupled models.',
-    image: '/images/risk.png',
+    image: '${process.env.PUBLIC_URL}/images/risk.png',
     link: '/research/flood'
   },
   {
     title: 'Projection of Heatwaves',
     description: 'High-resolution flood prediction using coupled models.',
-    image: '/images/heatwave_pdf.png',
+    image: '${process.env.PUBLIC_URL}/images/heatwave_pdf.png',
     link: '/research/flood'
   },
   {
     title: 'Hurricane Precipitation Climatology',
     description: 'High-resolution flood prediction using coupled models.',
-    image: '/images/hurricane_clim.png',
+    image: '${process.env.PUBLIC_URL}/images/hurricane_clim.png',
     link: '/research/flood'
   },
   {
     title: 'Coastal Risk Assessment',
     description: 'High-resolution flood prediction using coupled models.',
-    image: '/images/hurricane_clim.png',
+    image: '${process.env.PUBLIC_URL}/images/hurricane_clim.png',
     link: '/research/flood'
   },
   {
     title: 'Near Real-Time Forecasting of Hurricanes using AI models',
     description: 'High-resolution flood prediction using coupled models.',
-    image: '/images/hurricane_clim.png',
+    image: '${process.env.PUBLIC_URL}/images/hurricane_clim.png',
     link: '/research/flood'
   },
 ];
